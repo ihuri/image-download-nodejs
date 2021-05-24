63de40ec9b1f140660d682eb118e7aacc4f83301
Ihuri Carvalho Barboza <ihuri@outlook.com.br>
2021-05-24 14:10:10 -0300

    Update index.js
    Upl - Tratando extensões de imagens

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const fs = require('fs');
 // Parametros da Função URL, Diretório de destino, nome do arquivo
 async function download(url, dest, name) {
 
-    // Função para pegar o tipo do arquivo e concatena no final
-    dest = dest + '/' + name + url.substr(url.length - 4)
+    // Condição para pegar o tipo do arquivo e concatena no final
+    if (url.indexOf('.jpg') > 0) {
+        dest = dest + '/' + name + '.jpg';
+    } else if (url.indexOf('.png') > 0) {
+        dest = dest + '/' + name + '.png';
+    } else if (url.indexOf('.gif') > 0) {
+        dest = dest + '/' + name + '.gif';
+    } else {
+        dest = dest + '/' + name;
+    }
 
     /* Crie um arquivo vazio onde possamos salvar os dados */
     const file = fs.createWriteStream(dest);
@@ -33,5 +41,5 @@ async function download(url, dest, name) {
 
 //Exemplo
 (async() => {
-    const data = await download('https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png', './images', 'Image');
+    const data = await download('https://s2.glbimg.com/g3v1PypQU-zhZ-a4BICkEk3h9Qo=/850x446/s2.glbimg.com/-SD6jkXwv_MgNqyMHYw3dua7b0w=/0x178:695x521/695x343/s.glbimg.com/po/tt2/f/original/2015/05/31/nyan2.gif', './images', 'Gif');
 })();
